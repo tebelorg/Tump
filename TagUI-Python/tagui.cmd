71dd0afd0a73f6fd3dfd094142686f22910263c5
Ken Soh <kensoh@gmail.com>
2020-02-20 20:28:08 +0800

    stable delta files from TagUI cutting edge version

--- TagUI-Python/tagui.cmd
+++ TagUI-Python/tagui.cmd
@@ -678,13 +678,6 @@ if exist "tagui_no_logging" (
 	if exist "%flow_file%.js" del "%flow_file%.js"
 )
 
-rem hack chrome to prevent ended unexpectedly message
-set "chrome_pref=%LOCALAPPDATA%\Google\Chrome\User Data\Default"
-if exist "%chrome_pref%" (
-	gawk "sub(\"\\\"exited_cleanly\\\":false\", \"\\\"exited_cleanly\\\":true\")" "%chrome_pref%" > "%chrome_pref%" > nul 2>&1
-	gawk "sub(\"\\\"exit_type\\\":\\\"Crashed\\\"\", \"\\\"exit_type\\\":\\\"Normal\\\"\")" "%chrome_pref%" > "%chrome_pref%" > nul 2>&1
-)
-
 rem change back to initial directory where tagui is called
 cd /d "%initial_dir%"
 
